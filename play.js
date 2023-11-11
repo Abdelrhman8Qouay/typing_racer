a0056d0cbfd287773a70a3808ae05246cc595ec8
abdelrhman ashraf <mido59478@gmail.com>
2023-11-11 18:37:01 +0200

    almost almost ready done

--- play.js
+++ play.js
@@ -33,75 +33,105 @@ for(let i = 0; i < currentPara.length; i++) {
 const para_letters = document.querySelectorAll('.writerBox .back .letter');
 
 // play background audio
-writerBox.onload = playAudio(backgroundAudio, true);
+// writerBox.onload = playAudio(backgroundAudio, true);
+window.addEventListener('DOMContentLoaded', playAudio(backgroundAudio, true), false);
 
 // main options when play {a game racer typing}
 let content = currentPara.split(' '); // split all context to words separate by (space)
-let text_num = 0; // the text number [index]
 let char_num = 0; // the character number [index]
 let para_char_num = 0; // the total character numbers of paragraph as [index] {access on all chars in para}
 let timeout_game = 1.3; // timeout of the game is 1.3 minute [to get millisecond: minutes * 60 * 1000]
 let words_done = 0; // number of words done correctly
 
 let spaceKey = false;
+let delKey = false;
 
 // ======================== work on ========================
 textBackWriter.addEventListener('click', ()=> inputUser.focus());
 
 // check key pressed
-inputUser.addEventListener('keypress', (e)=> {
-    if(e.keyCode == 32) spaceKey = true;
-    else spaceKey = false;
+inputUser.addEventListener('keydown', (e)=> {
+    if(e.keyCode == 32) {
+        spaceKey = true;
+        delKey = false;
+    }
+    else if(e.keyCode == 8) {
+        delKey = true;
+        spaceKey = false;
+    }
+    else {
+        spaceKey = false;
+        delKey = false;
+    }
 })
 
 inputUser.addEventListener('input', (e)=> {
-
     // add audio sound press key
     let pressKeyAud = new Audio('./assets/sound/click.wav');
-    playAudio(pressKeyAud, false);
 
-    let old_char_num = char_num;
-    let input_char_index = e.target.value.length - 1;
+    let input_char_index = e.target.value.length;
     // for space key [change all game
     if(spaceKey) { // if pressed space >> 32
-        if(!e.target.value) { // if value empty '' {do nothing
+        if(!input_char_index) { // if value empty '' {do nothing
             if(hasClass(e.target, 'error')) removeClass(e.target, 'error');
+            playAudio(pressKeyAud, false);
         } else { // else
-            if(hasClass(e.target, 'error')) { // if contains (error class) {do nothing
-                false;
-            }else { // else {to continue typing racer with new word
-                if(e.target.value.trim() != content[text_num]) { // if input(trim >> removed spaces) != content[textIndex] {then make an error
-                    if(!hasClass(e.target, 'error')) addClass(e.target, 'error');
-                    if(!(old_char_num > input_char_index)) letterActive(char_num, 'error');
-                } else { // else {the space key is true then continue
-                    if(!(old_char_num > input_char_index)) letterActive(char_num, 'done');
-                    text_num++;
-                    e.target.value = '';
-                    char_num = input_char_index;
-                }
+            if(e.target.value.trim() != content[words_done]) { // if input(trim >> removed spaces) != content[textIndex] {then make an error
+                if(!hasClass(e.target, 'error')) clickError(e.target, 'error');
+                if(!delKey) letterActive(para_char_num, 'error');
+            } else { // else {the space key is true then continue
+                if(!delKey) letterActive(para_char_num, 'done');
+                words_done++;
+                para_char_num++;
+                e.target.value = '';
+                char_num = input_char_index;
+                playAudio(pressKeyAud, false);
             }
         }
-    }else { // else {for normal work
-        if(e.target.value[char_num] == content[text_num][char_num] && content[text_num].includes(e.target.value)) { // if value[charIndex] == content[textIndex][charIndex]
-            if(e.target.classList.contains('error')) e.target.classList.remove('error');
-            console.log('if char corr')
-            if(!(old_char_num > input_char_index)) letterActive(char_num, 'done');
+        console.log('space')
+    } else if(delKey) { // if delete key pressed {go back chars}
+        if(input_char_index < 0) { // if input.value not= empty {do nothing}
+            if(!hasClass(e.target, 'error')) removeClass(e.target, 'error');
+            false;
         } else { // else
-            e.target.classList.add('error');
-            console.log('if char err')
-            if(!(old_char_num > input_char_index)) letterActive(char_num, 'error');
+            // go back by classes
+            letterBack(para_char_num /*this current char_index*/, para_char_num - 1 /*this old char_index*/);
+            // if input.value[char before last] not= space {-para_char}
+            if(e.target.value[input_char_index - 1] != '&nbsp') {
+                para_char_num--;
+                console.log('whitespace here')
+            }
+            // remove {error input} if input true or input empty
+            if(content[words_done].includes(e.target.value) || !e.target.value) if(!hasClass(e.target, 'error')) removeClass(e.target, 'error');
+            // play sound
+            playAudio(pressKeyAud, false);
+            console.log('deleted key with value', para_char_num - 1)
         }
+        console.log('del')
+    } else { // else {that meaning normal key pressed
+        if(e.target.value[input_char_index -1] == content[words_done][input_char_index -1]) { // if value[charIndex] == content[textIndex][charIndex] [[[content[words_done].includes(e.target.value)]]]
+            letterActive(para_char_num, 'done');
+            playAudio(pressKeyAud, false);
+        } else { // else
+            clickError(e.target, 'error');
+        }
+        console.log('normal')
+        // then whatever go next
         char_num = input_char_index;
         para_char_num++;
     }
 
-    // when delete chars
-    if(old_char_num > input_char_index) letterBack(old_char_num, input_char_index);
+    console.log(para_char_num + ': para_char_num |', char_num + ': char_num |', words_done + ': words_done |', input_char_index + ': input_char_index;');
 })
 
-// console.log(para_letters)
 
 // ==================== Active functions ====================
+function clickError(el, className) {
+    addClass(el, className);
+    let pressKeyAud = new Audio('./assets/sound/err_click.mp3');
+    playAudio(pressKeyAud, false);
+}
+
 function letterActive(char_index, char_status) {
     if(char_index < 0) return;
     if(char_status == 'done') {
@@ -113,17 +143,19 @@ function letterActive(char_index, char_status) {
         removeClass(para_letters[char_index], 'letter-active')
         if(para_letters[char_index + 1]) addClass(para_letters[char_index + 1], 'letter-active');
     }
-    console.log(para_letters[char_index]);
+    // console.log(para_letters[char_index]);
 }
 function letterBack(currChar_index, toChar_index) {
     if(currChar_index < 0 || toChar_index < 0) return;
-    removeClass(para_letters[currChar_index], 'letter-active')
-    if(hasClass(para_letters[toChar_index], 'done')) {
-        removeClass(para_letters[currChar_index], 'done')
-        addClass(para_letters[currChar_index], 'letter-active')
-    } else if (para_letters[toChar_index], 'error') {
-        removeClass(para_letters[currChar_index], 'error')
-        addClass(para_letters[currChar_index], 'letter-active')
+    removeClass(para_letters[currChar_index], 'letter-active') // remove currentChar active
+    if(hasClass(para_letters[toChar_index], 'done')) { // if toChar done
+        removeClass(para_letters[toChar_index], 'done') // remove toChar done
+        addClass(para_letters[toChar_index], 'letter-active') // add toChar active
+        if(para_letters[currChar_index + 1]) if(hasClass(para_letters[currChar_index + 1], 'letter-active')) removeClass(para_letters[currChar_index + 1], 'letter-active')
+    } else if (para_letters[toChar_index], 'error') { // if toChar error
+        removeClass(para_letters[toChar_index], 'error') // remove toChar error
+        addClass(para_letters[toChar_index], 'letter-active') // add toChar active
+        if(para_letters[currChar_index + 1]) if(hasClass(para_letters[currChar_index + 1], 'letter-active')) removeClass(para_letters[currChar_index + 1], 'letter-active')
     }
 }
 
@@ -138,6 +170,10 @@ function playAudio(aud, audioLoop) {
     aud.preload = 'none';
     aud.play();
 }
+
+var containsWhitespace = function(str){
+	return /\s/g.test(str);
+};
 
 function hasClass(el, className)
 {
